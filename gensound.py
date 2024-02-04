a7b0010bdada019577b1537e3e79046fba3cff45
Than Thanpanit <boonyakorn.tha@gmail.com>
2024-02-03 18:40:56 -0800

    remove source files

--- gensound.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-
-nextround = gTTS('เปลี่ยนฐาน', lang='th')
-timeout = gTTS('หมดเวลา', lang='th')
-oneminleft = gTTS('เหลือเวลาอีก 1 นาที', lang='th')
-
-nextround.save('nextround.mp3')
-timeout.save('timeout.mp3')
-oneminleft.save('oneminleft.mp3')
